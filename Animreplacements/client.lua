530200eaaded8223136bc1666d2181a1e7d160f6
Maximus7474 <the.maximus.7474@gmail.com>
2025-03-18 20:22:34 +0100

    tweak(client): comment out debug code

--- Animreplacements/client.lua
+++ Animreplacements/client.lua
@@ -5,16 +5,17 @@ AddStateBagChangeHandler('WeaponAnim', nil, function(bagName, key, value, reserv
         local playerped = GetPlayerPed(playerIdx)
 
 
-        if value then
-            CreateThread(function ()
-                local ti = GetGameTimer() + 1000 * 60
-                while GetGameTimer() < ti do
-                    local coords = GetEntityCoords(playerped)
-                    DrawMarker(2, coords.x, coords.y, coords.z + 0.98, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.2, -0.15, 30, 150, 30, 222, false, false, 0, true, false, false, false)
-                    Wait(0)
-                end
-            end)
-        end
+        --[[ Debug code to show which peds are affected by the override ]]
+        -- if value then
+        --     CreateThread(function ()
+        --         local ti = GetGameTimer() + 1000 * 60
+        --         while GetGameTimer() < ti do
+        --             local coords = GetEntityCoords(playerped)
+        --             DrawMarker(2, coords.x, coords.y, coords.z + 0.98, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.2, -0.15, 30, 150, 30, 222, false, false, 0, true, false, false, false)
+        --             Wait(0)
+        --         end
+        --     end)
+        -- end
 
         SetWeaponAnimationOverride(playerped, value and "customanim" or `Default`)
         print(('Updated animation override for ^4%s^7'):format(bagName), value and "customanim" or "Default", playerIdx, id)
